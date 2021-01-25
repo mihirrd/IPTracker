761b5e53ddd1ae24661bc86ac40b2d21e4b3e044
mihirrd <deshpande.mihir7@gmail.com>
2021-01-25 14:45:15 +0530

    search bar working, displaying the data correctly

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './Main.module.css'
-import axios from 'axios'
 
-export class Main extends Component {
 
-    state = {
-        ip : "",
-    }
+function Main(props) {
 
-    componentDidMount = async () =>{
-        axios.get('https://geo.ipify.org/api/v1?apiKey=at_PzHVdj7RtKgtUenDZXRvshAFG2XDU&ipAddress=103.146.240.30')
-        .then((respose)=>console.log(respose.data))
-        .catch(error => console.log(error))
-    }
-
-    render() {
-        return (
-            <div className = {styles.box}>
-                <div className = {styles.ip}>IP ADDRESS</div>
-                <div className = {styles.loc}>LOCATION</div>
-                <div className = {styles.tzone}>TIME ZONE</div>
-                <div className = {styles.isp}>ISP</div>
+    return (
+        <div className = {styles.box}>
+            <div className = {styles.ip}>
+                <h4>IP ADDRESS</h4>
+                <h2>{props.ip === "" ? "---" : props.ip}</h2>
             </div>
-        )
-    }
+            <div className = {styles.loc}>
+                <h4 className = {styles.location}>LOCATION</h4>
+                <p className = {styles.city}>{props.city}</p>
+                <p className = {styles.region}>{props.region}</p>
+                <p className = {styles.country}>{props.country}</p>    
+            </div>
+            <div className = {styles.tzone}>
+                <h4>TIME ZONE</h4>
+                <h1>{props.tzone}</h1>
+                
+            </div>
+            <div className = {styles.isp}>
+                <h4>ISP</h4>
+                <h1>{props.isp}</h1>
+                
+            </div>
+        </div>
+    )
 }
 
 export default Main
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, {Component}from 'react'
 import styles from './Searchbar.module.css'
 import Main from '../Main/Main.js'
+import axios from 'axios'
 
-function Searchbar() {
-
+class Searchbar extends Component {
     
-    return (
-        <div className = {styles.bg}>
-            <div className = {styles.searchcontainer}>
-                <input className = {styles.bar} type="text" name="search" placeholder="Search.."/>
-                <button className = {styles.button}> > </button>
+    state = {
+        ip : "",
+        loc : {},
+        isp : "",
+        
+        
+    }
+    
+    
+    handleChange =(event)=>{
+        const {value} = event.target
+        this.setState({ip : value})
+        
+    }
+    handleSearch = async()=>{
+        const url = `https://geo.ipify.org/api/v1?apiKey=at_PzHVdj7RtKgtUenDZXRvshAFG2XDU&ipAddress=${this.state.ip}`
+        let fetchedData = {}
+        await axios.get(url).then(res => fetchedData = res.data)
+        .catch(e => console.log(e))
+
+        this.setState({
+            loc : fetchedData.location,
+            isp : fetchedData.isp 
+        })
+    }
+
+    render(){
+        
+        return (
+            <div className = {styles.bg}>
+                <div className = {styles.searchcontainer}>
+                    <input 
+                        className = {styles.bar} 
+                        type="text" value = {this.state.ip} 
+                        name="search" placeholder="Search.."
+                        onChange = {this.handleChange}
+                        />
+                    <button 
+                    className = {styles.button} 
+                    onClick = {this.handleSearch}
+                    > {'>'} </button>
+                </div>
+                <Main 
+                    country = {this.state.loc.country} 
+                    ip = {this.state.ip}
+                    region = {this.state.loc.region}
+                    city = {this.state.loc.city}
+                    tzone = {this.state.loc.timezone}
+                    isp = {this.state.isp}
+                    />
             </div>
-            <Main/>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default Searchbar
